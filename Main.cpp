493e1f231e2258b9ae0f23e18a96a844b79e76e7
Hunter <ripsomgardih@merrimack.edu>
2014-10-16 18:43:17 -0400

    Initialization
    putting out files

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+/* Main.cpp
+* Author: Hunter Ripsom-Gardiner
+* Date: 10/16/14 
+* Course: CSC 5210 Graphics
+* Description: A program that 
+*/
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/freeglut.h>
+#include <iostream>
+#include <cstdio>
+// Local Utilities Includes
+#include "vgl.h"
+#include "vec.h"
+#include "LoadShaders.h"
